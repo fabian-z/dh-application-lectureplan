aca57a9a9554c4c3ada97cb1e6ea777c362301d7
Fabian Zaremba <fabian@youremail.eu>
2021-06-02 01:05:41 +0200

    First bootstrap base template

--- backend/template/template.go
+++ backend/template/template.go
@@ -11,6 +11,6 @@ type Templates struct {
 
 func (t *Templates) Init(path string) error {
 	var err error
-	t.Base, err = template.ParseFiles(filepath.Join(path, "base.html"))
+	t.Base, err = template.ParseFiles(filepath.Join(path, "base.html"), filepath.Join(path, "nav.html"), filepath.Join(path, "footer.html"))
 	return err
 }
