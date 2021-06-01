7b0caac4da789d3d0a90987efb31a25a191fa664
Fabian Zaremba <fabian@youremail.eu>
2021-06-02 01:24:29 +0200

    Install fullcalendar NPM packages

--- frontend/src/js/main.js
+++ frontend/src/js/main.js
@@ -9,6 +9,26 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import "../css/main.css";
 
+// npm install @fullcalendar/core @fullcalendar/daygrid @fullcalendar/timegrid @fullcalendar/list
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import interactionPlugin from '@fullcalendar/interaction';
+import bootstrapPlugin from '@fullcalendar/bootstrap'
+import scrollgridPlugin from '@fullcalendar/scrollgrid'
+
+let calendar = new Calendar(calendarEl, {
+  plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin, bootstrapPlugin, scrollgridPlugin ],
+  initialView: 'dayGridMonth',
+  headerToolbar: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'dayGridMonth,timeGridWeek,listWeek'
+  }
+});
+
+
 document.addEventListener('DOMContentLoaded', function() {
     // Setup handlers when DOM is ready
 
